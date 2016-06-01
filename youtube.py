8a2bea03ea20732044a4c93cc2daaec530a00e62
Levi Dooley <i.am.stickfigure@gmail.com>
2016-06-01 17:13:20 -0500

    Add files via upload

--- youtube.py
+++ youtube.py
@@ -10,35 +10,39 @@ headers = {
         'cache-control': "no-cache"
     }
 
+def channel_scan(username=None, channelID=None):
+    # "id": "UCvQECJukTDE2i6aCoMnS-Vg",
+    querystring = {
+            "part": "contentDetails",
+            "forUsername": username,
+            "key": api_key
+        }
+    channel_response = requests.request("GET", channels_url, headers=headers, params=querystring)
+    print(channel_response.text)
 
-# "id": "UCvQECJukTDE2i6aCoMnS-Vg",
-querystring = {
-        "part": "contentDetails",
-        "forUsername": "bigthink",
-        "key": api_key
-    }
-channel_response = requests.request("GET", channels_url, headers=headers, params=querystring)
-print(channel_response.text)
-
-playlistId = channel_response.items[0].contentDetails.relatedPlaylists.uploads
-
-
-# "pageToken": "CDIQAA",
-# "playlistId": "UUvQECJukTDE2i6aCoMnS-Vg",
-querystring = {
-        "part": "contentDetails",
-        "playlistId": playlistId,
-        "maxResults": "4",
-        "key": api_key
-    }
-playlist_response = requests.request("GET", playlistItems_url, headers=headers, params=querystring)
-print(playlist_response.text)
+    playlistId = channel_response.json()["items"][0]["contentDetails"]["relatedPlaylists"]["uploads"]
 
 
-querystring = {
-        "videoId": "3oz7k7Wn_vo",
-        "part": "id",
-        "key": api_key
-    }
-response = requests.request("GET", captions_url, headers=headers, params=querystring)
-print(response.text)
+    # "pageToken": "CDIQAA",
+    # "playlistId": "UUvQECJukTDE2i6aCoMnS-Vg",
+    querystring = {
+            "part": "contentDetails",
+            "playlistId": playlistId,
+            "maxResults": "50",
+            "key": api_key
+        }
+    playlist_response = requests.request("GET", playlistItems_url, headers=headers, params=querystring)
+    print(playlist_response.text)
+
+    print("========================================================================================")
+
+    videos = playlist_response.json()["items"]
+
+    for video in videos:
+        querystring = {
+                "videoId": video["contentDetails"]["videoId"],
+                "part": "id",
+                "key": api_key
+            }
+        response = requests.request("GET", captions_url, headers=headers, params=querystring)
+        print(response.text)
